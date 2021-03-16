8cb90abb9b96fedcdf8716b5580354e70b75d137
Justyna Książek <jus.ksiazek1998@gmail.com>
2021-03-16 10:48:49 +0100

    del

--- site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"pl.polsl.lab.justyna.ksiazek.controller"},{"l":"pl.polsl.lab.justyna.ksiazek.model"}];updateSearchResults();
